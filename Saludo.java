b413fa6faecf00b7ff3d2488923f0ce1921c9af9
Miriam <miriammartinezgonzalez2001@gmail.com>
2024-12-10 16:51:50 +0100

    Creado Saludo.java

---
+++ Saludo.java
@@ -0,0 +1,18 @@
+public class Saludo {
+    public static void saludar (){
+
+        System.out.println("Buenos dias");
+        
+    }
+    public static void despedida (){
+
+        System.out.println("Adiós");
+    }
+    
+    public static void main (String [] args){
+    
+        saludar();
+        despedida();
+     
+    }
+}
